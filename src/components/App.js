6f3c5b49531c73addd45c29a63bcde23fc8681b1
René van der Stok <rene.vander.stok@kvk.nl>
2020-04-06 11:39:38 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <div>Message Board: you have 0 new messages </div>;
+}
+
+export default App;

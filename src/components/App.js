f397bf68060232251f99d0d79e4889a75afcc8d8
paulinekors <paulinekors@gmail.com>
2020-04-06 17:29:28 +0200

    review, comments added to README and App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React from "react";
 
 function App() {
   return <div>Message Board: you have 0 new messages </div>;
